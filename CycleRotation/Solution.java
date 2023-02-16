00c4c71ca7053908887cbc15dfc4a96e7b38ad2e
Lau2689 <105868708+Lau2689@users.noreply.github.com>
2023-02-16 12:50:47 +0100

    Update Solution.java

--- CycleRotation/Solution.java
+++ CycleRotation/Solution.java
@@ -1,1 +1,18 @@
-.
+class Solution {
+    public int[] solution(int[] A, int K) {    
+        return rotatingArrayElemente(A,K);          
+    }
+
+    public int[] rotatingArrayElemente (int[] A, int K){
+        int i;
+        while (K > 0){            
+            int lastArrayElement = A[A.length - 1];
+            for (i = A.length - 1; i > 0; i--) {
+                A[i] = A [i - 1];
+            }
+            A[i] = lastArrayElement;
+            K--;
+        }
+        return A;       
+    }
+}
