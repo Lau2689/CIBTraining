98fdb2fa5e90bcc0f75272fcf201afcf48788a6b
Lau2689 <105868708+Lau2689@users.noreply.github.com>
2023-02-16 13:48:40 +0100

    Update Solution.java

--- TimeComplexity/FrogJMP/Solution.java
+++ TimeComplexity/FrogJMP/Solution.java
@@ -1,1 +1,14 @@
-.
+class Solution {
+    public int solution(int X, int Y, int D) {
+        return countingJumps(X, Y, D);
+    }
+
+    public int countingJumps(int currentPosition, int    finalPosition, int jumpDistance){
+        int sideToSideDistance = finalPosition - currentPosition ;
+        int numberOfJumps = sideToSideDistance/jumpDistance;
+        if (numberOfJumps < sideToSideDistance) {
+            numberOfJumps += 1;
+        }
+        return numberOfJumps;
+    }
+}
