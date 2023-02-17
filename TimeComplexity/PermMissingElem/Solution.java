93e639c95c8029e60359c670e005033cab2117d7
Lau2689 <105868708+Lau2689@users.noreply.github.com>
2023-02-17 10:10:08 +0100

    Update Solution.java
    hay dos códigos de momento para ir viendo la mejor opción para cubrir lo casos y se irá modificando hasta tener el definitivo

--- TimeComplexity/PermMissingElem/Solution.java
+++ TimeComplexity/PermMissingElem/Solution.java
@@ -15,3 +15,38 @@ class Solution {
         return missingNumber;
     }
 }
+
+//Intentando cubrir casos. Revisar
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] A) {
+        return findingMissingNumber (A);
+    }
+    public static int findingMissingNumber(int[] givenArray){
+        Arrays.sort (givenArray);
+        int missingNumber = 0;
+        int i = 0;
+        if(givenArray.length > 2) {
+            for (i = givenArray.length - 1; i > 0; i--) {
+                if (givenArray[i] - 1 != givenArray[i - 1]) {
+                    missingNumber = givenArray[i - 1] + 1;
+                    return missingNumber;
+                }
+            }
+        }
+        if (givenArray.length == 0){
+            missingNumber = 0;
+            return  missingNumber;
+        }
+        if (givenArray.length == 1){
+            missingNumber =givenArray[0];
+            return  missingNumber;
+        }
+        if (givenArray.length == 2){
+            missingNumber = givenArray[givenArray[i] -1] -1;
+            return  missingNumber;
+        }
+
+        return missingNumber;
+    }
+}
