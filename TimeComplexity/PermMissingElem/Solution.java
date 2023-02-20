eee6637bb6163fbc6ce82e0919bbb0ddd4cfc2f8
Lau2689 <105868708+Lau2689@users.noreply.github.com>
2023-02-20 09:38:16 +0100

    Update Solution.java
    este código pasa el 100% de los test, pero ahora voy a dividirlo en distintos métodos

--- TimeComplexity/PermMissingElem/Solution.java
+++ TimeComplexity/PermMissingElem/Solution.java
@@ -1,50 +1,57 @@
 import java.util.Arrays;
-class Solution {
-    public int solution(int[] A) {
-        return findingMissingNumber (A);
-    }
-    public static int findingMissingNumber(int[] givenArray){
-        Arrays.sort (givenArray);
-        int missingNumber = 0;
-         for (int i = givenArray.length - 1; i > 0 ; i--){
-             if (givenArray[i] - 1 != givenArray[i - 1] ){
-                 missingNumber = givenArray[i - 1] + 1;
-                 return missingNumber;
-             }
-         }
-        return missingNumber;
-    }
-}
 
-//Intentando cubrir casos. Revisar
-import java.util.Arrays;
 class Solution {
     public int solution(int[] A) {
-        return findingMissingNumber (A);
+        // Implement your solution here
+        return findingMissingNumber(A);
     }
-    public static int findingMissingNumber(int[] givenArray){
-        Arrays.sort (givenArray);
+    public static int findingMissingNumber(int[] givenArray) {
+        Arrays.sort(givenArray);
         int missingNumber = 0;
-        int i = 0;
-        if(givenArray.length > 2) {
-            for (i = givenArray.length - 1; i > 0; i--) {
-                if (givenArray[i] - 1 != givenArray[i - 1]) {
-                    missingNumber = givenArray[i - 1] + 1;
-                    return missingNumber;
-                }
+
+        if (givenArray.length == 0) {
+            missingNumber = 1;
+            return missingNumber;
+        }
+        if (givenArray.length == 1) {
+            if (givenArray[0] == 1) {
+                missingNumber = 2;
+            } else {
+                missingNumber = 1;
+            }
+            return missingNumber;
+        }
+        if (givenArray.length == 2) {
+            if (givenArray[0] != 1) {
+                missingNumber = 1;
+                return missingNumber;
+            }
+            if (givenArray[1] == givenArray[0] + 1) {
+                missingNumber = givenArray[1] + 1;
+                return missingNumber;
+
+            } else {
+                missingNumber = givenArray[0] + 1;
+                return missingNumber;
             }
         }
-        if (givenArray.length == 0){
-            missingNumber = 0;
-            return  missingNumber;
-        }
-        if (givenArray.length == 1){
-            missingNumber =givenArray[0];
-            return  missingNumber;
-        }
-        if (givenArray.length == 2){
-            missingNumber = givenArray[givenArray[i] -1] -1;
-            return  missingNumber;
+        if (givenArray.length > 2) {
+            if (givenArray[0] == 1) {
+                for (int i = 1; i < givenArray.length; i++) {
+                    if (givenArray[i] != givenArray[i - 1] + 1) {
+                        missingNumber = givenArray[i] - 1;
+                        break;
+
+                    } else {
+                        missingNumber = givenArray[givenArray.length - 1] + 1;
+                    }
+
+                }
+                return missingNumber;
+            } else {
+                missingNumber = 1;
+                return missingNumber;
+            }
         }
 
         return missingNumber;
