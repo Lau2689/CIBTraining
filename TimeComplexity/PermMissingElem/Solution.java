749c77ed8507b9814e0e03d858dd67616ff08523
Lau2689 <105868708+Lau2689@users.noreply.github.com>
2023-02-20 09:58:35 +0100

    Dividido en métodos. Pasa 90%
    Sin dividir en métodos me pasaba el 100%. Ahora, dividido, me pasa el 90%. Me da este error:
    
    empty_and_single
    empty list and single element✘RUNTIME ERROR
    tested program terminated with exit code 1
    1.0.008 sRUNTIME ERROR, tested program terminated with exit code 1
    stderr:
    Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 0
    	at Solution.findingMissingNumberWhenArrayLengthLongerThanTwo(Solution.java:49)
    	at Solution.solution(Solution.java:17)
    	at Exec.run(exec.java:46)
    	at Exec.main(exec.java:35)

--- TimeComplexity/PermMissingElem/Solution.java
+++ TimeComplexity/PermMissingElem/Solution.java
@@ -1,59 +1,68 @@
 import java.util.Arrays;
 
 class Solution {
+    int missingNumber;
+    
     public int solution(int[] A) {
         // Implement your solution here
-        return findingMissingNumber(A);
+        if (A.length == 0){
+            findingMissingNumberWhenArrayLengthZero (A);
+        }
+        if (A.length == 1) {
+            findingMissingNumberWhenArrayLengthOne(A);
+        }
+        if (A.length == 2){
+            findingMissingNumberWhenArrayLengthTwo(A);
+        }
+        else {
+            findingMissingNumberWhenArrayLengthLongerThanTwo(A);
+        }
+        return missingNumber;
     }
-    public static int findingMissingNumber(int[] givenArray) {
-        Arrays.sort(givenArray);
-        int missingNumber = 0;
-
-        if (givenArray.length == 0) {
+    
+    public int findingMissingNumberWhenArrayLengthZero (int[] givenArray){        
+        return  missingNumber = 1;
+    }
+    
+    public int findingMissingNumberWhenArrayLengthOne (int[] givenArray){
+        if (givenArray[0] == 1) {
+            missingNumber = 2;
+        } else {
+            missingNumber = 1;
+        }
+        return missingNumber;
+    }
+    
+    public  int findingMissingNumberWhenArrayLengthTwo (int[] givenArray){
+        Arrays.sort (givenArray);
+        if (givenArray[0] != 1) {
             missingNumber = 1;
             return missingNumber;
         }
-        if (givenArray.length == 1) {
-            if (givenArray[0] == 1) {
-                missingNumber = 2;
-            } else {
-                missingNumber = 1;
-            }
+        if(givenArray[1] == givenArray[0]+1){
+            missingNumber = givenArray[1]+1;
+            return missingNumber;
+        }else {
+            missingNumber = givenArray[0] + 1;
             return missingNumber;
         }
-        if (givenArray.length == 2) {
-            if (givenArray[0] != 1) {
-                missingNumber = 1;
-                return missingNumber;
-            }
-            if (givenArray[1] == givenArray[0] + 1) {
-                missingNumber = givenArray[1] + 1;
-                return missingNumber;
-
-            } else {
-                missingNumber = givenArray[0] + 1;
-                return missingNumber;
-            }
-        }
-        if (givenArray.length > 2) {
-            if (givenArray[0] == 1) {
-                for (int i = 1; i < givenArray.length; i++) {
-                    if (givenArray[i] != givenArray[i - 1] + 1) {
-                        missingNumber = givenArray[i] - 1;
-                        break;
-
-                    } else {
-                        missingNumber = givenArray[givenArray.length - 1] + 1;
-                    }
-
+    }
+    
+    public int findingMissingNumberWhenArrayLengthLongerThanTwo (int[] givenArray) {
+        Arrays.sort(givenArray);
+        if (givenArray[0] == 1) {
+            for (int i = 1; i < givenArray.length; i++) {
+                if (givenArray[i] != givenArray[i - 1] + 1) {
+                    missingNumber = givenArray[i] - 1;
+                    break;
+                } else {
+                    missingNumber = givenArray[givenArray.length - 1] + 1;
                 }
-                return missingNumber;
-            } else {
-                missingNumber = 1;
-                return missingNumber;
             }
+            return missingNumber;
+        } else {
+            missingNumber = 1;
+            return missingNumber;
         }
-
-        return missingNumber;
     }
 }
