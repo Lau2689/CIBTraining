a64d2d21de22645c71eb8263c3923df2d1b9e60f
Lau2689 <105868708+Lau2689@users.noreply.github.com>
2023-02-27 16:43:16 +0100

    Mejorado, pero debe cambiar
    ahora pasa un 33% en vez de 22%, pero hay runtime errors y timeout errors. Borrar comentarios y hacer código más limpio

--- CountingElements/MissingInteger/Solution.java
+++ CountingElements/MissingInteger/Solution.java
@@ -1,20 +1,15 @@
-// you can also use imports, for example:
-// import java.util.*;
-
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
 import java.util.*;
 class Solution {
     public int solution(int[] A) {
         // Implement your solution here
         return findingSmallestPositiveInteger(A);
-    }
+   }
     public static int findingSmallestPositiveInteger(int[] givenArray) {
         int smallestPositiveInteger= 0;
         List <Integer> positiveNumbers = new ArrayList<>();
         List <Integer> negativeNumbers = new ArrayList<>();
-        int i = 0;
-        int j = 0;
+        int i;
+        int j;
 
         for(i = 0; i < givenArray.length; i++) {
             //separo positivos de negativos
@@ -27,11 +22,11 @@ class Solution {
                     if(positiveNumbers.size() > 1){
                         for (j = 1; j < positiveNumbers.size(); j ++) {
                             //todos los numeros presentes, se devuelve el ultimo número + 1
-                            if (positiveNumbers.get (i) - 1 == positiveNumbers.get(i - 1) || positiveNumbers.get(i) == positiveNumbers.get(i - 1)){
+                            if (positiveNumbers.get (j) - 1 == positiveNumbers.get(j - 1) || positiveNumbers.get(j) == positiveNumbers.get(j - 1)){
                                 smallestPositiveInteger = positiveNumbers.get(positiveNumbers.size() - 1) + 1;
                             //si falta algun numero, se devuelve ese numero
                             } else{
-                                smallestPositiveInteger = positiveNumbers.get(i) - 1;
+                                smallestPositiveInteger = positiveNumbers.get(j) - 1;
                                 break;
                             }
                         }
@@ -45,7 +40,7 @@ class Solution {
                     if(positiveNumbers.size() > 1){
                         //todos los numeros presentes
                         if (positiveNumbers.get (i) - 1 == positiveNumbers.get(i - 1) || positiveNumbers.get(i) == positiveNumbers.get(i - 1)){
-                            smallestPositiveInteger = positiveNumbers.get(0) - 1;
+                            smallestPositiveInteger = 1;
                         //falta algún numero
                         }else{
                             smallestPositiveInteger = positiveNumbers.get(i) - 1;
