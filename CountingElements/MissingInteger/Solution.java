32175bb6858d95fc1817edca62a96de5b8bc88a4
Lau2689 <105868708+Lau2689@users.noreply.github.com>
2023-02-28 12:04:36 +0100

    55%. Sigo revisando. Hacer Tests

--- CountingElements/MissingInteger/Solution.java
+++ CountingElements/MissingInteger/Solution.java
@@ -1,62 +1,50 @@
 import java.util.*;
 class Solution {
-    public int solution(int[] A) {
-        // Implement your solution here
-        return findingSmallestPositiveInteger(A);
-   }
-    public static int findingSmallestPositiveInteger(int[] givenArray) {
-        int smallestPositiveInteger= 0;
-        List <Integer> positiveNumbers = new ArrayList<>();
-        List <Integer> negativeNumbers = new ArrayList<>();
-        int i;
-        int j;
+    int smallestPositiveInteger= 0;
+    List <Integer> positiveNumbers = new ArrayList<>();
+    int i; 
 
-        for(i = 0; i < givenArray.length; i++) {
-            //separo positivos de negativos
-            if (givenArray[i] > 0){
-                positiveNumbers.add(givenArray[i]);  //lista de positivos
-                Collections.sort(positiveNumbers);   //lista ordenada
-                //lista empieza por 1
-                if (positiveNumbers.get(0) == 1){
-                    //longitud mayor de 1
-                    if(positiveNumbers.size() > 1){
-                        for (j = 1; j < positiveNumbers.size(); j ++) {
-                            //todos los numeros presentes, se devuelve el ultimo número + 1
-                            if (positiveNumbers.get (j) - 1 == positiveNumbers.get(j - 1) || positiveNumbers.get(j) == positiveNumbers.get(j - 1)){
-                                smallestPositiveInteger = positiveNumbers.get(positiveNumbers.size() - 1) + 1;
-                            //si falta algun numero, se devuelve ese numero
-                            } else{
-                                smallestPositiveInteger = positiveNumbers.get(j) - 1;
-                                break;
-                            }
-                        }
-                    //longitud igual a 1
-                    } else{
-                         smallestPositiveInteger = positiveNumbers.get (i) + 1;
-                    }
-                //lista no empieza por 1
-                }else{
-                    //longitud mayor que 1
-                    if(positiveNumbers.size() > 1){
-                        //todos los numeros presentes
-                        if (positiveNumbers.get (i) - 1 == positiveNumbers.get(i - 1) || positiveNumbers.get(i) == positiveNumbers.get(i - 1)){
-                            smallestPositiveInteger = 1;
-                        //falta algún numero
-                        }else{
-                            smallestPositiveInteger = positiveNumbers.get(i) - 1;
-                        }
-                    //longitud igual a 1
-                    } else{
-                        smallestPositiveInteger = positiveNumbers.get (i) - 1;
-                    }
-                }
-            //lista de numero negativos
-            } else {
-                negativeNumbers.add(givenArray[i]);
-                smallestPositiveInteger =  1;
-            }
-        }
-        System.out.println(smallestPositiveInteger);
-        return smallestPositiveInteger;
+    public int solution(int[] A) {
+        return findingSmallestPositiveInteger(A);
     }
-}
+    
+    public int findingSmallestPositiveInteger(int[] givenArray) {
+        Arrays.sort(givenArray);
+        if ((givenArray[0] <= 0 && givenArray[givenArray.length - 1] >0) || (givenArray[0] >= 0 && givenArray[givenArray.length - 1] > 0)){
+            separatingPositiveAnNegativeNumbers(givenArray);
+            if (positiveNumbers.get(0) == 1) {
+                findingSmallestPositiveIntegerWhenStartsWithOne(positiveNumbers);
+            }else{
+                smallestPositiveInteger =1;
+            }
+        }else{
+            smallestPositiveInteger = 1;    
+        }
+        return smallestPositiveIntege;
+    }
+    
+    public List<Integer> separatingPositiveAnNegativeNumbers(int[] givenArray){
+        for (i = 0; i < givenArray.length; i++) {            
+            if (givenArray[i] > 0) {
+                positiveNumbers.add(givenArray[i]);
+            }
+        }        
+        return positiveNumbers;   
+    }
+    
+    public int findingSmallestPositiveIntegerWhenStartsWithOne(List <Integer> positiveNumbers){         
+        if (positiveNumbers.size() > 1) {
+            for (i = 1; i < positiveNumbers.size(); i++) {                
+                if (positiveNumbers.get(i) - 1 == positiveNumbers.get(i - 1) || positiveNumbers.get(i) == positiveNumbers.get(i - 1)) {
+                    smallestPositiveInteger = positiveNumbers.get(positiveNumbers.size() - 1) + 1;                   
+                } else {
+                    smallestPositiveInteger = positiveNumbers.get(i) - 1;
+                    break;
+                }
+            }          
+        } else {
+            smallestPositiveInteger = positiveNumbers.get(0) + 1;
+        }
+        return smallestPositiveInteger;  
+    }
+} 
