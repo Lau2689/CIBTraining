76236e1ed39f704590f30a44c4da53677f4bd515
Lau2689 <105868708+Lau2689@users.noreply.github.com>
2023-03-22 15:03:48 +0100

    modified

--- Sorting/MaxProductOfThree/MaxProductOfThreeTests.java
+++ Sorting/MaxProductOfThree/MaxProductOfThreeTests.java
@@ -2,6 +2,8 @@ package com.example.codilityExercices;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class MaxProductOfThreeTests {
     private final MaxProductOfThree maxProductOfThree = new MaxProductOfThree();
 
