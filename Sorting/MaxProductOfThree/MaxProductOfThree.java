38b7c6492e497e153491f724405dcff2c5433f1c
Lau2689 <105868708+Lau2689@users.noreply.github.com>
2023-03-22 15:05:02 +0100

    100%

--- Sorting/MaxProductOfThree/MaxProductOfThree.java
+++ Sorting/MaxProductOfThree/MaxProductOfThree.java
@@ -3,10 +3,19 @@ package com.example.codilityExercices;
 import java.util.Arrays;
 
 public class MaxProductOfThree {
-    int maxProduct;
+    int maxProductFromLastPosition;
+    int maxProductoFromFirstPosition;
+    int theMaxProduct
+        
     public int solution (int[] A){
         Arrays.sort(A);
         maxProduct = A[A.length - 1] * A[A.length - 2] * A[A.length - 3];
+        maxProductoFromFirstPosition = A[0] * A[1] * A[A.length - 1];
+        if (maxProductFromLastPosition > maxProductoFromFirstPosition) {
+            theMaxProduct = maxProductFromLastPosition;
+        }else{
+            theMaxProduct = maxProductoFromFirstPosition;
+        }
         return maxProduct;
     }
 }
