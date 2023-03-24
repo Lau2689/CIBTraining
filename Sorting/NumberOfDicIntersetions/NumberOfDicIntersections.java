bb69b5f4eb43873577aeb6bb68d47d7faf348f9c
Lau2689 <105868708+Lau2689@users.noreply.github.com>
2023-03-24 12:20:53 +0100

    Refactored

--- Sorting/NumberOfDicIntersetions/NumberOfDicIntersections.java
+++ Sorting/NumberOfDicIntersetions/NumberOfDicIntersections.java
@@ -14,23 +14,36 @@ public class NumberOfDicIntersections {
     List <int[]> allPairs = new ArrayList<>();
 
     public int solution(int[] A){
+         return findingNumberOfDicIntersections(A);
+    }
+    private int findingNumberOfDicIntersections (int [] A){
         for (i = 0; i < A.length; i++) {
-            minPointCircle1 = Long.valueOf(i) - Long.valueOf(A[i]);
-            maxPointCircle1 = Long.valueOf(i) + Long.valueOf(A[i]);
-            ValueRange circleRange = ValueRange.of(minPointCircle1,maxPointCircle1);            
+            calculatingFirstAndLastPointOfFirstCircleDiameter(A);
+            ValueRange circleRange = ValueRange.of(minPointCircle1,maxPointCircle1);
             for (j = i +1; j < A.length; j++ ){
-                minPointCircle2 = Long.valueOf(j) - Long.valueOf(A[j]);
-                maxPointCircle2 = Long.valueOf(j) + Long.valueOf(A[j]);
-                for (long values2 = minPointCircle2; values2 <= maxPointCircle2; values2++ ) {
-                    if (circleRange.isValidValue(values2)) {
-                        int [] pair = {i,j};
-                        allPairs.add(pair);
-                        break;
-                    }
-                }
-            }           
+                calculatingFirstAndLastPointOfSecondCircleDiameter(A);
+                creatingPairsList(circleRange);
+            }
         }
         numberOfIntersections = allPairs.size();
         return numberOfIntersections;
+    }
+
+    private void calculatingFirstAndLastPointOfFirstCircleDiameter(int [] A){
+        minPointCircle1 = Long.valueOf(i) - Long.valueOf(A[i]);
+        maxPointCircle1 = Long.valueOf(i) + Long.valueOf(A[i]);
+    }
+    private void calculatingFirstAndLastPointOfSecondCircleDiameter(int [] A){
+        minPointCircle2 = Long.valueOf(j) - Long.valueOf(A[j]);
+        maxPointCircle2 = Long.valueOf(j) + Long.valueOf(A[j]);
+    }
+    private void creatingPairsList (ValueRange circleRange){
+        for (long values2 = minPointCircle2; values2 <= maxPointCircle2; values2++ ) {
+            if (circleRange.isValidValue(values2)) {
+                int [] pair = {i,j};
+                allPairs.add(pair);
+                break;
+            }
+        }
     }
 }
