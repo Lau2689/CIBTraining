5b13cd4db4fc54019a4a1e6773bd7e61297c9925
Lau2689 <105868708+Lau2689@users.noreply.github.com>
2023-03-27 15:25:22 +0200

    83%. 100%correcteness, 50% performance

--- Leader/Dominator/Dominator.java
+++ Leader/Dominator/Dominator.java
@@ -1,24 +1,28 @@
 package com.example.codilityExercices;
 
 public class Dominator {
-
     int dominatorPositions;
     int counter;
 
     public int solution(int[] A){
-        dominatorPositions = -1;
         int j;
+        if (A.length == 0){
+            dominatorPositions = -1;
+        }
         for (int i = 0; i < A.length; i ++){
             int dominator = i;
-            for (j = i; j < A.length; j ++){
+
+            for (j = i + 1; j < A.length; j ++){
                 if (A[j] == A[i]){
                     counter += 1;
                 }
             }
-            if (counter > A.length/2) {
+            if (counter + 1 > A.length/2) {
                 dominatorPositions = dominator;
                 break;
             }
+            else dominatorPositions = -1;
+            counter = 0;
         }
         return dominatorPositions;
     }
