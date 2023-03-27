675fb349bb1e1221098b7db7ecabfed36348c2ff
Lau2689 <105868708+Lau2689@users.noreply.github.com>
2023-03-27 15:19:15 +0200

    Update DominatorTests.java

--- Leader/Dominator/DominatorTests.java
+++ Leader/Dominator/DominatorTests.java
@@ -3,16 +3,45 @@ package com.example.codilityExercices;
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DominatorTests {
     private final Dominator dominator = new Dominator();
 
     @Test
-    public void codilityExample (){
+    public void whenADominatorExistsAndOnlyOneRepeatedNumber (){
         int[] A = {3,4,3,2,3,-1,3,3};
         int result = dominator.solution(A);
         List<Integer> positions = Arrays.asList(0, 2, 4, 6, 7);
         assertTrue(positions.contains(result));
+    }
+
+    @Test
+    public void whenADominatorExistsAndMultipleRepeatedNumbers (){
+        int[] A = {3,4,3,2,2, 3,-1,3,3};
+        int result = dominator.solution(A);
+        List<Integer> positions = Arrays.asList(0, 2, 4, 7, 8);
+        assertTrue(positions.contains(result));
+    }
+    @Test
+    public void whenThereIsNotADominator(){
+        int[] A = {2,1,1,3};
+        int result = dominator.solution(A);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void whenThereIsNotADominatorAndFirstHalfIsTheSAmeNumber(){
+        int[] A = {2,2,2,2,2,2,2,2,2,2,3,5,7,7,8,6,5,1,1,3};
+        int result = dominator.solution(A);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void givenAEmptyArray(){
+        int[] A = {};
+        int result = dominator.solution(A);
+        assertEquals(-1, result);
     }
 }
